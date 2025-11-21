0de5edc59088f7c5d5fadfd79876138addb9ae1d
Znsim <133223311+Znsim@users.noreply.github.com>
2025-11-21 09:27:17 +0900

    Change resultText from '뿡' to '앙'

--- frontend/lib/pages/main_web.dart
+++ frontend/lib/pages/main_web.dart
@@ -14,7 +14,7 @@ class MainPageWeb extends StatefulWidget {
 class _MainPageWebState extends State<MainPageWeb> {
   final TextEditingController _textController = TextEditingController();
   Uint8List? _imageBytes;
-  String resultText = "뿡";
+  String resultText = "앙";
 
   Future<void> _pickImage() async {
     final picked = await ImagePicker().pickImage(source: ImageSource.gallery);
